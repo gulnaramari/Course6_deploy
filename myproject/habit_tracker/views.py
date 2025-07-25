62459775848b4967a084a183379a2cf7881320e6
gulna <gulnaramari@yandex.ru>
2025-07-25 15:18:00 +0300

    CRUD и пагинация для приложения habit_tracker

--- myproject/habit_tracker/views.py
+++ myproject/habit_tracker/views.py
@@ -1,6 +1,35 @@
 from rest_framework import generics
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
+from .models import Habit
+from .paginators import HabitPaginator
 from .serializers import HabitSerializer
+from users.permissions import IsOwner
+
+
+class HabitsListAPIView(generics.ListAPIView):
+    """Класс для эндпоинта списка привычек."""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
+
+    def get_queryset(self):
+        """Метод для изменения запроса  к базе данных (фильтрации по объектам модели привычки)"""
+
+        user = self.request.user
+        if user.is_authenticated:
+            return Habit.objects.filter(Q(owner=user) | Q(is_public=True)).order_by('id')
+        else:
+            return Habit.objects.filter(is_public=True).order_by('id')
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """Класс для эндпоинта просмотра привычки."""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
@@ -10,9 +39,23 @@ class HabitCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        """Метод вносит изменение в сериализатор создания "Привычки".
-        присваивает привычку владельцу"""
+        """Метод присваивает привычку владельцу"""
 
         new_habit = serializer.save()
         new_habit.owner = self.request.user
         new_habit.save()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Класс для эндпоинта изменения привычки."""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """Класс для эндпоинта удаления привычки."""
+
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
