bb6c37f1b412f7a36df5d3fa0278aa7f391f787b
gulna <gulnaramari@yandex.ru>
2025-07-25 15:36:48 +0300

    CRUD и маршрутизация для приложения users

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,24 +1,27 @@
 from django.urls import path
-from .apps import UsersConfig
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserListAPIView
+from .apps import UsersConfig
+from .views import (
+    UserListAPIView,
+    UserRetrieveAPIView,
+    UserCreateAPIView,
+    UserUpdateAPIView,
+    UserDestroyAPIView,
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
-app_name = UsersConfig
-
+app_name = UsersConfig.name
 
 urlpatterns = [
-    path("", UserListAPIView.as_view(), name="profiles"),
-
-    path(
-        "authorization/",
-        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
-        name="authorization",
-    ),
-    path(
-        "refresh/",
-        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
-        name="token_refresh",
-    ),
+    path("", UserListAPIView.as_view(), name="list"),
+    path("detail/<int:pk>/", UserRetrieveAPIView.as_view(), name="detail"),
+    path("registration/", UserCreateAPIView.as_view(), name="registration"),
+    path("detail/<int:pk>/update/", UserUpdateAPIView.as_view(), name="update_detail"),
+    path("detail/<int:pk>/delete/", UserDestroyAPIView.as_view(), name="delete_detail"),
+    path("authorization/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="authorization"),
+    path("refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
 ]
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,13 +1,66 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import User
-
-from .serializer import UserSerializer
+from .permissions import IsUserOwner
+from .serializer import (
+    UserSerializer,
+    CreateUserBaseSerializer,
+    UserBaseSerializer,
+)
 
 
 class UserListAPIView(generics.ListAPIView):
-    """Контроллер-дженерик для списка пользователей."""
+    """Класс для эндпоинта списка пользователей."""
+
+    serializer_class = UserBaseSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """Класс для эндпоинта просмотра деталей о пользователе."""
 
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        """Метод получения сериализатора в соответствии с запросом."""
+
+        if (
+            self.request.method == "GET"
+            and self.get_object() != self.request.user
+            or self.request.user.is_superuser is False
+        ):
+            return UserBaseSerializer
+        if self.request.user.is_superuser:
+            return UserSerializer
+        return UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Класс для эндпоинта создания пользователя."""
+
+    serializer_class = CreateUserBaseSerializer
+
+    def perform_create(self, serializer):
+        """Метод вносит изменение в сериализатор создания "Пользователя"."""
+
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """Класс для эндпоинта редактирования деталей пользователя."""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsUserOwner]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    """Класс для эндпоинта удаления пользователя."""
+
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsUserOwner]
