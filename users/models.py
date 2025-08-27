6b36834db3f85ca519148304a4382efab22c4580
gulnaramari <gulnaramari@yandex.ru>
2025-08-27 22:01:10 +0300

    init feature deploy

--- users/models.py
+++ users/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Поле 'email' обязательно для заполнения.")
